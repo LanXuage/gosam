779389b911a115e2631ee6ff077f57c77ee9b65d
lanxuage <zx1456817554@gmail.com>
2023-03-15 17:51:22 +0800

    Add darwin & windows.
    Signed-off-by: lanxuage <zx1456817554@gmail.com>

--- gscan/common/helper.go
+++ gscan/common/helper.go
@@ -3,6 +3,7 @@ package common
 import (
 	"log"
 	"net"
+	"runtime"
 	"strings"
 	"syscall"
 )
@@ -15,27 +16,34 @@ func GetOuiPrefix(mac net.HardwareAddr) (string, string) {
 
 func GetGateways() []net.IP {
 	ret := []net.IP{}
-	netlinks, err := syscall.NetlinkRIB(syscall.RTM_GETROUTE, syscall.AF_INET)
-	if err != nil {
-		log.Fatal(err)
-	}
-	nmsg, err := syscall.ParseNetlinkMessage(netlinks)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, m := range nmsg {
-		if m.Header.Type == syscall.RTM_NEWROUTE {
-			attrs, err := syscall.ParseNetlinkRouteAttr(&m)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, attr := range attrs {
-				if attr.Attr.Type == syscall.RTA_GATEWAY {
-					ret = append(ret, attr.Value)
+	if runtime.GOOS == `linux` {
+		netlinks, err := syscall.NetlinkRIB(syscall.RTM_GETROUTE, syscall.AF_INET)
+		if err != nil {
+			log.Fatal(err)
+		}
+		nmsg, err := syscall.ParseNetlinkMessage(netlinks)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, m := range nmsg {
+			if m.Header.Type == syscall.RTM_NEWROUTE {
+				attrs, err := syscall.ParseNetlinkRouteAttr(&m)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, attr := range attrs {
+					if attr.Attr.Type == syscall.RTA_GATEWAY {
+						ret = append(ret, attr.Value)
+					}
 				}
 			}
 		}
+	} else if runtime.GOOS == `windows` {
+
+	} else if runtime.GOOS == `darwin` {
+
 	}
+
 	return ret
 }
 
