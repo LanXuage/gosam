edbc2de7b1bc7b19801fa225adfe6ebb81fa4170
lanxuage <zx1456817554@gmail.com>
2023-03-15 18:01:07 +0800

    Add darwin & windows.
    Signed-off-by: lanxuage <zx1456817554@gmail.com>

--- gscan/common/helper.go
+++ gscan/common/helper.go
@@ -1,50 +1,14 @@
 package common
 
 import (
-	"log"
 	"net"
-	"runtime"
 	"strings"
-	"syscall"
 )
 
 func GetOuiPrefix(mac net.HardwareAddr) (string, string) {
 	ret1 := strings.ToUpper(strings.Replace(mac.String()[:8], ":", "", -1))
 	ret2 := strings.ToUpper(strings.Replace(mac.String()[:13], ":", "", -1))
 	return ret1, ret2
-}
-
-func GetGateways() []net.IP {
-	ret := []net.IP{}
-	if runtime.GOOS == `linux` {
-		netlinks, err := syscall.NetlinkRIB(syscall.RTM_GETROUTE, syscall.AF_INET)
-		if err != nil {
-			log.Fatal(err)
-		}
-		nmsg, err := syscall.ParseNetlinkMessage(netlinks)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, m := range nmsg {
-			if m.Header.Type == syscall.RTM_NEWROUTE {
-				attrs, err := syscall.ParseNetlinkRouteAttr(&m)
-				if err != nil {
-					log.Fatal(err)
-				}
-				for _, attr := range attrs {
-					if attr.Attr.Type == syscall.RTA_GATEWAY {
-						ret = append(ret, attr.Value)
-					}
-				}
-			}
-		}
-	} else if runtime.GOOS == `windows` {
-
-	} else if runtime.GOOS == `darwin` {
-
-	}
-
-	return ret
 }
 
 func IP2Uint32(ip net.IP) uint32 {
